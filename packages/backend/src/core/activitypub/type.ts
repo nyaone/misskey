d3aa2401ba9479d053dc75f285b11fcee43ef750
Nya Candy <dev@candinya.com>
2024-01-26 14:21:50 +0800

    fix(edit): add missing `updated` field

--- packages/backend/src/core/activitypub/type.ts
+++ packages/backend/src/core/activitypub/type.ts
@@ -119,6 +119,7 @@ export interface IPost extends IObject {
 	_misskey_quote?: string;
 	_misskey_content?: string;
 	quoteUrl?: string;
+	updated?: Date;
 }
 
 export interface IQuestion extends IObject {
