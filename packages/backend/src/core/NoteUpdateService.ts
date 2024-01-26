7bca3d8a0409714609257ea5e25382a4cca75b6d
Nya Candy <dev@candinya.com>
2024-01-26 13:00:07 +0800

    debug: print rendered content into console log

--- packages/backend/src/core/NoteUpdateService.ts
+++ packages/backend/src/core/NoteUpdateService.ts
@@ -82,7 +82,10 @@ export class NoteUpdateService {
 						await this.apRendererService.renderNote(newNote, false), newNote,
 					),
 				);
-				
+
+				// Add temporarily debug logs, simply print to console
+				console.log(JSON.stringify(content, null, 2));
+
 				this.deliverToConcerned(user, newNote, content);
 			}
 		}
