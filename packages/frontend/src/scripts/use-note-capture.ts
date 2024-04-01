9d1b25ef13339f53a4bbd09d17954c3078ce7404
Nya Candy <dev@candinya.com>
2024-04-01 20:59:17 +0800

    chore: print debug output for note update

--- packages/frontend/src/scripts/use-note-capture.ts
+++ packages/frontend/src/scripts/use-note-capture.ts
@@ -74,6 +74,7 @@ export function useNoteCapture(props: {
 			}
 
 			case 'updated': {
+				console.log(JSON.stringify(note.value)); // Debug output
 				note.value.history = [
 					...note.value.history,
 					{
