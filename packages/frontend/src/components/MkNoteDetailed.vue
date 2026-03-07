24406f62e742c563799becdd553a3acee5500853
Nya Candy <dev@candinya.com>
2026-03-07 10:12:29 +0800

    fix: onMounted is not defined

--- packages/frontend/src/components/MkNoteDetailed.vue
+++ packages/frontend/src/components/MkNoteDetailed.vue
@@ -240,7 +240,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { computed, inject, markRaw, provide, ref, useTemplateRef } from 'vue';
+import { computed, onMounted, inject, markRaw, provide, ref, useTemplateRef } from 'vue';
 import * as mfm from 'mfm-js';
 import * as Misskey from 'misskey-js';
 import { isLink } from '@@/js/is-link.js';
